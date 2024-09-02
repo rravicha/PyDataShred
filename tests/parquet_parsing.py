c139ca2c05eca7f01d37519f6d32575fb99f1ec1
snehurashu <49406916+snehurashu@users.noreply.github.com>
2024-09-02 23:13:12 +0530

    Update parquet_parsing.py

--- tests/parquet_parsing.py
+++ tests/parquet_parsing.py
@@ -1,6 +1,33 @@
+import sys, os
+sys.path.append('/workspaces/PyDataShred/')
+
 from datashredpy.helper.data import Data
 from datashredpy.helper.enums import FileType
-# from ..PyDataShred.datashredpy.data import Data
 
-df=Data.read("C:/Users/497523/Downloads/MT cars.parquet",FileType.PARQUET)
-print(df)
+
+#df=Data.read("/workspaces/PyDataShred/tests_data/MT cars.parquet",FileType.PARQUET)
+#print(df)
+
+#pip3 install pyarrow
+import pandas as pd
+def test_read_parquet():
+    df = Data.read('/workspaces/PyDataShred/tests_data/MT cars.parquet', FileType.PARQUET)
+    print(df)
+#    print(df.columns)
+#    print(df.shape)
+    try:
+        assert True, 'first'
+        assert 7 == 7, 'second'
+        assert 1 == 1, 'third'
+        assert isinstance(df, pd.DataFrame),'fourth'
+        assert list(df.columns) == ['model', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am',
+       'gear', 'carb'],'fifth'
+        assert df.shape == (32,12),'sixth'
+        assert df.loc[17, 'model'] == 'Fiatt 128','seventh'
+    except AssertionError as e:
+        print(f'Assertion failed at the {str(e)} line')
+    exit(1)
+    
+
+if __name__=='__main__':
+    test_read_parquet()
