59b2369b39a18813cb87441fea463fc6f5348355
Adityaislearning <Shuklaaditya009@gmail.com>
2024-09-02 08:23:27 +0000

    commit

--- datashredpy/helper/data.py
+++ datashredpy/helper/data.py
@@ -6,7 +6,7 @@ class Data:
 
     @classmethod
     def _read_csv(cls,rel_path:str, file_type: FileType, **options) ->  Pandas.DataFrame:
-        return Pandas.read_csv(rel_path,**options)
+        return Pandas.read_csv(rel_path,**options,header=0)
 
     @classmethod
     def read(cls, rel_path: str, file_type: FileType, use_pandas: bool = True, use_spark: Optional[bool] = True, **options):
@@ -14,4 +14,10 @@ class Data:
             if use_pandas:
                 return cls._read_csv(rel_path, file_type, **options)
     
+        if file_type==FileType.TXT:
+            if use_pandas:
+                return cls._read_csv(rel_path, file_type, **options)
 
+        if file_type==FileType.TSV:
+            if use_pandas:
+                return cls._read_csv(rel_path, file_type, **options)
