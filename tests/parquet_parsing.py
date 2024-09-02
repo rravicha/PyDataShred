f6f83590b8162f73e1c1ef06f83b9792759f8a58
snehurashu <49406916+snehurashu@users.noreply.github.com>
2024-09-02 22:08:19 +0530

    Delete tests/parquet_parsing.py

--- tests/parquet_parsing.py
+++
@@ -1,6 +0,0 @@
-from datashredpy.helper.data import Data
-from datashredpy.helper.enums import FileType
-# from ..PyDataShred.datashredpy.data import Data
-
-df=Data.read("C:/Users/497523/Downloads/MT cars.parquet",FileType.PARQUET)
-print(df)
