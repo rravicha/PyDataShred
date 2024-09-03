875dfcf3b22084aea43ef210a285ac9383af3e5e
ashishsbp <52271591+ashishsbp@users.noreply.github.com>
2024-09-03 11:04:34 +0530

    Update delta_file_read.py

--- tests/delta_file_read.py
+++ tests/delta_file_read.py
@@ -1,14 +1,16 @@
+from datashredpy.helper.data import Data
+from datashredpy.helper.enums import FileType
 import pandas as pd
 from deltalake import DeltaTable
  
  
-def read_delta_file_to_dataframe(delta_file_path: r"C:\Python\My_Project\delta_file") -> pd.DataFrame:
+def read_delta_file_to_dataframe(delta_file_path: r"tests_data\emp_delta_format/employee_data") -> pd.DataFrame:
     delta_file = DeltaTable(delta_file_path)
     df = delta_file.to_pandas()
     return df
  
 if __name__ == "__main__":
-    delta_file_path = r"C:\Python\My_Project\delta_file"
+    delta_file_path = r"tests_data\emp_delta_format/employee_data"
     # Read the Delta Lake table and get the DataFrame
     df = read_delta_file_to_dataframe(delta_file_path)
     # Print the DataFrame
