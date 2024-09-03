2d37fd380ec53f634d2319d416c9e9bd1ff4dead
ashishsbp <52271591+ashishsbp@users.noreply.github.com>
2024-09-03 11:06:48 +0530

    Update delta_file_read.py

--- tests/delta_file_read.py
+++ tests/delta_file_read.py
@@ -4,13 +4,13 @@ import pandas as pd
 from deltalake import DeltaTable
  
  
-def read_delta_file_to_dataframe(delta_file_path: r"tests_data\emp_delta_format/employee_data") -> pd.DataFrame:
+def read_delta_file_to_dataframe(delta_file_path: r"tests_data/emp_delta_format/employee_data") -> pd.DataFrame:
     delta_file = DeltaTable(delta_file_path)
     df = delta_file.to_pandas()
     return df
  
 if __name__ == "__main__":
-    delta_file_path = r"tests_data\emp_delta_format/employee_data"
+    delta_file_path = r"tests_data/emp_delta_format/employee_data"
     # Read the Delta Lake table and get the DataFrame
     df = read_delta_file_to_dataframe(delta_file_path)
     # Print the DataFrame
