fd780742efd3c59465ec28874457231f109e4109
Raghavendran R <41452724+rravicha@users.noreply.github.com>
2024-09-11 22:40:02 +0000

    something big2

--- tests_data/data_generator.py
+++ tests_data/data_generator.py
@@ -11,3 +11,23 @@ with sessionmaker(bind=engine)() as session:
     for _ in range(10):
         session.add(my_table(column1=fake.random_int(), column2=fake.sentence()))
     session.commit()
+
+from sqlalchemy import create_engine, Column, Integer, String, Table, MetaData
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+pymysql://user:password@host/database')
+metadata = MetaData()
+
+# Define your table structure
+my_table = Table('your_table', metadata,
+    Column('column1', Integer),
+    Column('column2', String)
+)
+
+# Create the table (if it doesn't exist)
+metadata.create_all(engine)
+
+# Insert mock data
+with sessionmaker(bind=engine)() as session:
+    session.add(my_table(column1=1, column2='mock data'))
+    session.commit()
